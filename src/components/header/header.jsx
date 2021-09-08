a32dcaf42aef81faedca79373345b4bc7062f824
Andrey Khaylov <andrey.haylovv@yandex.ru>
2021-09-08 17:13:10 +0300

    Создан header компонент

---
+++ src/components/header/header.jsx
@@ -0,0 +1,19 @@
+import s from './header.module.css';
+
+function Header({ title, desc }) {
+    return (
+        <header className={s.root}>
+            <div className={s.forest}></div>
+            <div className={s.container}>
+                {
+                   title && <h1>{title}</h1>
+                }
+                {
+                   desc && <p>{desc}</p>
+                }
+            </div>
+        </header>
+    )
+}
+
+export default Header;
