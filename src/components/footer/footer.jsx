6ad7355f56b9641611d203ad1a8b69c892ba5f3d
Andrey Khaylov <andrey.haylovv@yandex.ru>
2021-09-08 18:59:42 +0300

    Создан компонент footer

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,14 @@
+import s from './footer.module.css'
+
+function Footer () {
+  return (
+    <footer>
+      <div className={s.wrapper}>
+        <h3>THANKS FOR VISITING</h3>
+        <p>2021 #Card Game.</p>
+      </div>      
+    </footer>
+  )
+}
+
+export default Footer
