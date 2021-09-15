345f9fcdd0e09c46ae31182429f174a51c8811b6
Andrey Khaylov <andrey.haylovv@yandex.ru>
2021-09-15 16:44:33 +0300

    Добавлены стили в header компонент

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -12,6 +12,8 @@ function Header({ title, desc }) {
     return (
         <header className={s.root}>
             <div className={s.forest}></div>
+            <div className={s.silhouette}></div>
+            <div className={s.moon}></div>
             <div className={s.container}>
                 <h1>{title}</h1>
                 <p>{desc}</p>
