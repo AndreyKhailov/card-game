68371390967edefc1029ba879210bc3bbab7e806
Andrey Khaylov <andrey.haylovv@yandex.ru>
2021-09-15 12:34:14 +0300

    Добавлены экспорты about, contacts, notFound в routes index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,2 +1,5 @@
 export { default as Home } from './home/home';
 export { default as Game } from './game/game';
+export { default as about } from './about/about';
+export { default as contacts } from './contacts/contacts';
+export { default as notFound } from './notFound/notFound';
