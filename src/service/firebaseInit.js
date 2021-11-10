c1542138cdedba042627a196660b30191714e0a6
Andrey Khaylov <andrey.haylovv@yandex.ru>
2021-11-10 22:50:21 +0300

    изменен firebase под запросы REAST API

--- src/service/firebaseInit.js
+++ src/service/firebaseInit.js
@@ -1,48 +1,63 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/database';
+// import firebase from 'firebase/compat/app';
+// import 'firebase/compat/database';
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyA7NgFky_QwA2UvwovY0Dry1qg1NjtoTuU',
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyA7NgFky_QwA2UvwovY0Dry1qg1NjtoTuU',
 
-  authDomain: 'card-game-edbf6.firebaseapp.com',
+//   authDomain: 'card-game-edbf6.firebaseapp.com',
 
-  databaseURL: 'https://card-game-edbf6-default-rtdb.firebaseio.com',
+//   databaseURL: 'https://card-game-edbf6-default-rtdb.firebaseio.com',
 
-  projectId: 'card-game-edbf6',
+//   projectId: 'card-game-edbf6',
 
-  storageBucket: 'card-game-edbf6.appspot.com',
+//   storageBucket: 'card-game-edbf6.appspot.com',
 
-  messagingSenderId: '1081065656480',
+//   messagingSenderId: '1081065656480',
 
-  appId: '1:1081065656480:web:c91653ee970cc2f229b9c5',
-};
+//   appId: '1:1081065656480:web:c91653ee970cc2f229b9c5',
+// };
 
-firebase.initializeApp(firebaseConfig);
+// firebase.initializeApp(firebaseConfig);
 
 class FireBase {
   constructor() {
-    this.fire = firebase;
-    this.database = this.fire.database();
+    this.host = `https://card-game-fa17c-default-rtdb.firebaseio.com/`;
+    this.localID = null;
   }
 
-  getCardSoket = (cb) => {
-    this.database.ref('cards').on('value', (snapshot) => cb(snapshot.val()));
+  token = () => localStorage.getItem('idToken');
+
+  setLocalID = (localId) => {
+    this.localID = localId;
   };
 
-  getCardsOnce = async () => {
-    return await this.database
-      .ref('cards')
-      .once('value')
-      .then((snapshot) => snapshot.val());
+  checkLocalID() {
+    if (!this.localID) {
+      throw {
+        msg: 'LocalID is does not exist',
+      };
+    }
+  }
+
+  getCards = async () => {
+    try {
+      this.checkLocalID();
+
+      const res = await fetch(`${this.host}/${this.localID}/cards.json?auth=${this.token()}`);
+
+      return res;
+    } catch (e) {
+      console.log('error', e);
+    }
   };
 
-  offCardsOnce = () => {
-    this.database.ref('cards').off();
-  };
+  addCard = async (data) => {
+    const res = await fetch(`${this.host}/${this.localID}/cards.json?auth=${this.token()}`, {
+      method: 'POST',
+      body: JSON.stringify(data),
+    }).then((res) => res.json());
 
-  setCard = (card) => {
-    const newKey = this.database.ref().child('pokemons').push().key;
-    this.database.ref('cards/' + newKey).set(card);
+    return res;
   };
 }
 
