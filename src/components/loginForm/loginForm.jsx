97198f214bd27599c269d56eb0a9c824ce460df8
Andrey Khaylov <andrey.haylovv@yandex.ru>
2021-11-16 13:40:14 +0300

    сделана валидация формы

--- src/components/loginForm/loginForm.jsx
+++ src/components/loginForm/loginForm.jsx
@@ -1,7 +1,9 @@
 import { useState } from 'react';
+import { NotificationManager } from 'react-notifications';
 
 import { Button } from '../../components';
 import useInput from './hooks/useInput';
+import userSchema from './validation/userSchema';
 
 import s from './loginForm.module.css';
 
@@ -12,46 +14,58 @@ function LoginForm({ onSubmit = f => f }) {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        
-        onSubmit({
+
+        const data = {
             email: emailProps.value, 
-            password: passwordProps.value, 
-            isSignIn,
+            password: passwordProps.value,
+        };
+
+        userSchema.isValid(data).then((valid) => {
+            if (valid) {
+                onSubmit({ ...data, isSignIn });
+                resetEmail('');
+                resetPassword('');
+            } else {
+                NotificationManager.error(valid, 'Некорректный email или пароль!');
+            };
         });
-        resetEmail('');
-        resetPassword('');
     };
 
-    const handleLogin = () => {
+    const handleLogin = (e) => {
+        e.preventDefault();
         setSignIn(prevState => !prevState);
     };
+
+    const handleBtn = (emailProps.value.trim() && passwordProps.value.trim())
 
     return (
         <form onSubmit={handleSubmit}>
             <div className={s.root}>
                 <input 
-                    type="email" 
+                    type="text" 
                     className={s.input}
                     {...emailProps}
                     required
                 />
                 <span className={s.highlight}></span>
                 <span className={s.bar}></span>
+                <span className={s.example}>Например: example@example.com</span>
                 <label className={s.label}>e-mail</label>
             </div>
             <div className={s.root}>
                 <input 
-                    type="password" 
+                    type="text" 
                     className={s.input}
                     {...passwordProps}
                     required
                 />
                 <span className={s.highlight}></span>
                 <span className={s.bar}></span>
+                <span className={s.example}>Пароль должен содержать от 6 до 25 символов</span>
                 <label className={s.label}>Пароль</label>
             </div>
             <div className={s.form_btn}>
-                <Button>
+                <Button isDisabled={!handleBtn}>
                     { isSignIn ? 'Войти' : 'Создать' }
                 </Button>
                 <button 
